42b20f3fd9b088ff83e5f37c36c7e49f4f403483
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2024-05-06 11:47:44 +0545

    Initial Commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,46 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{
+    generate_context, Builder, CustomMenuItem, Manager, SystemTray, SystemTrayEvent,
+    SystemTrayMenu, WindowEvent,
+};
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
+    Builder::default()
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                // toggle application window
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .on_window_event(|event| match event.event() {
+            WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
+        .run(generate_context!())
+        .expect("error while running tauri application");
+}
